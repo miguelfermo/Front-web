98fac9d6f2013cc7499effbbd1bd58acc370e567
Eryc Jacinto <121405251+ErycMJ@users.noreply.github.com>
2024-06-15 17:58:09 -0300

    Update NavBarEdit.jsx
    Disponibilizado rotas para login e pagina inicial

--- src/Components/ErycComponents/NavBarEdit.jsx
+++ src/Components/ErycComponents/NavBarEdit.jsx
@@ -1,20 +1,27 @@
 import logo from "../../assets/logoName.png"
 import SearchEdit from "./SearchEdit"
+import imgperfil from "../../assets/iconPerfil.png"
+import { Link } from 'react-router-dom';
 
 export default function NavBarEdit(){
   return (
     <div className="navBar flex justify-between items-center searchDiv max-h-32	gap-2 bg-greyIsh rounded-[10px] p-[3rem]">
       <div className="logoDiv">
-        <img className="h-16" src={logo} alt="logo" />
+        <button>
+        <Link to="/" className="link">
+          <img className="h-16" src={logo} alt="logo" />
+        </Link>
+        </button>
       </div>
       <div>
         <SearchEdit />
       </div>
       <div className="menu flex gap-5">
-        <li className="menuList text-[#6f6f6f] hover:text-orange-600">Login</li>
-        <li className="menuList text-[#6f6f6f] hover:text-orange-600">
-          Register
-        </li>
+      <div className="flex items-center justify-center mb-4">
+        <Link to="/login" className="link">
+            <img src={imgperfil} className='perfil' alt="icone perfil" />
+        </Link>
+        </div>
       </div>
     </div>
   )
