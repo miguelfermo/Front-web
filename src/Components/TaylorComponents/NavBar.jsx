819ed86db9fd721d85472738fdeacc38892af3fc
Taylor Vargas Teixeira <taylorvargasteixeira@gmail.com>
2024-06-15 17:36:10 -0300

    Link to login

--- src/Components/TaylorComponents/NavBar.jsx
+++ src/Components/TaylorComponents/NavBar.jsx
@@ -1,16 +1,18 @@
 import logo from "../../assets/logoName.png"
+import { Link } from "react-router-dom"
 
 const NavBar = () => {
   return (
     <div className="navBar flex justify-between items-center p-[1rem]">
-      <div className="logoDiv">
+      <Link to="/Donations">
         <img className="h-16" src={logo} alt="logo" />
-      </div>
-
+      </Link>
       <div className="menu flex gap-8">
-        <li className="menuList text-[#6f6f6f] hover:text-orange-600">Login</li>
         <li className="menuList text-[#6f6f6f] hover:text-orange-600">
-          Register
+          <Link to="/login">Login</Link>
+        </li>
+        <li className="menuList text-[#6f6f6f] hover:text-orange-600">
+          <Link to="/login">Register</Link>
         </li>
       </div>
     </div>
