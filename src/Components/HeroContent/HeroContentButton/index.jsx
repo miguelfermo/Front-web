e8808a816fbed2914ad9a434c2fb1949d0b3576a
erycmj <erycmj@gmail.com>
2026-06-17 20:13:28 -0300

    fix: change hero screen colors

--- src/Components/HeroContent/HeroContentButton/index.jsx
+++ src/Components/HeroContent/HeroContentButton/index.jsx
@@ -4,7 +4,7 @@ export function HeroContentButton() {
     return (
         <div className="flex justify-center m-4">
             <Link to="/Donations">
-                <button className="w-72 rounded-lg bg-yellow-600 py-2 text-2xl font-bold tracking-widest text-yellow-900 shadow-md transition-colors duration-200 hover:bg-orange-800">
+                <button className="w-72 rounded-lg bg-yellow-600 py-2 text-2xl font-bold tracking-widest text-yellow-900 shadow-md transition-colors duration-200 hover:bg-yellow-800 focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:ring-offset-2">
                     NAVEGAR
                 </button>
             </Link>
